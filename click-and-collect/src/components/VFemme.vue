08b5c6793f17d0f88d0b43021b46c21f6196025b
BaptisteDantas <baptistedantaspro@gmail.com>
2020-11-23 15:16:49 +0100

    dossierProjet

--- click-and-collect/src/components/VFemme.vue
+++ click-and-collect/src/components/VFemme.vue
@@ -1,20 +1,26 @@
 <template>
 <div>
+  <h1 style="margin-top: 30px">Notre boutique de parfum pour les femmes</h1>
 <div class="container">    
   <div class="row">
     <div id="border" class="col-md-5">
       <div class="panel panel-primary">
-        <div class="panel-heading">Overdose des ailes</div>
-        <div class="panel-body"><img src="..\assets\Femmes\01-l-overdose-des-ailes.jpg" height="174" width="174" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer"><router-link class="" to="/">50 mobiles and get a gift card</router-link></div>
+        <div class="panel-heading">Gabrielle de Chanel - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\06-gabrielle-de-chanel.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Composée par Olivier Polge, Gabrielle de Chanel essence est la fragrance d’une fleur imaginaire, facettée de jasmin, d’ylang-ylang, de fleur d’oranger et de la plus envoûtante de toutes les fleurs, la tubéreuse de grasse. Intensément féminine, Gabrielle de Chanel essence est la fragrance d’une femme solaire.</div>
+        <div><strong>Prix: 74€</strong></div>
       </div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="..\assets\Femmes\03-Parfum-du-colibri.png" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer"></div>
+        <div class="panel-heading">La vie est belle de Lancôme - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\19-la-vie-est-Belle.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer" style="padding-top: 30px">Parfum de jasmin d'Arabie, de fleur d'oranger et d'essence de patchouli.
+          Embellissez votre vie avec La vie est belle, le parfum féminin iconique de Lancôme.</div>
+        <div style="margin-top: 40px"><strong>Prix: 62€</strong></div>
       </div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
   
   </div>
@@ -24,21 +30,46 @@
   <div class="row">
     <div id="border" class="col-md-5">
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="..\assets\Femmes\08-la-petite-robe-noir.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">Découvrez l'Eau de toilette La Petite Robe Noire.
-          Ma Robe Cocktail
-          Dans le dressing parfumé de La Petite Robe Noire, je suis la Robe Cocktail</div>
+        <div class="panel-heading">Trésor de Lancôme - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\20-Tresor-Lancome.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">L'amour est un trésor. Depuis sa création en 1990, l'Eau de parfum Trésor incarne l'amour absolu sous toutes ses facettes. Une fragrance subtile et légère qu'enveloppe un voile doux et romantique</div>
       </div>
-      <div>Prix : 150€</div>
+      <div style="margin-top: 80px"><strong>Prix: 120€</strong></div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="..\assets\Femmes\09-Shalimar.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">prix: <strong>15€</strong></div>
+        <div class="panel-heading">Olympéa de Paco Rabanne - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\24-Olympea-PacoRabanne.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Déesse des temps modernes, Un parfum oriental frais, puissant, porté par un accord "vanille salée" qui adore la peau. Et un duel frais-charnel : impulsion florale avec la mandarine verte, le jasmin d'eau et la fleur de gingembre. Effet "peau" de la Vanille salée. Un parfum pour femme sensuel, exponentiel, presque animal. Chauffé par l'ambregris et le bois de Cachemire. Fatal.</div>
+        <div style="margin-top: 10px"><strong>Prix: 59€</strong></div>
       </div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
+    </div>
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div id="border" class="col-md-5">
+      <div class="panel panel-primary">
+        <div class="panel-heading">J'adore de Dior - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\25-j-adore-dior.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">J’adore Eau de parfum de Dior : une fragrance mythique, véritable best seller de la Maison. J'adore compte parmi les parfums les plus vendus de la maison Dior</div>
+      </div>
+      <div style="margin-top: 70px"><strong>Prix: 45€</strong></div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
+    </div>
+    <div id="border" class="col-md-5"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading">La petite robe en noire - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Femmes\08-la-petite-robe-noir.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer" style="padding-top: 30px">Dans le dressing parfumé de La Petite Robe Noire, je suis la Première Robe.
+          Une création parfumée signée Guerlain : une eau de parfum brodée des plus belles notes "noires" de la parfumerie : Cerise noire, Rose noire, Patchouli, Thé noir.
+          </div>
+        <div style="margin-top: 15px"><strong>Prix: 69€</strong></div>
+      </div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
   </div>
 </div><br>
@@ -49,6 +80,10 @@
 </template>
 
 <style>
+
+.container {
+  padding-left: 150px;
+}
   #border {
     margin-right: 30px;
     margin-top: 50px;
