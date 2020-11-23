c23eb4ed6bcf3cc1c22ecea07af1decfcc2cc8bc
BaptisteDantas <baptistedantaspro@gmail.com>
2020-11-23 13:48:49 +0100

    dossierProjet

---
+++ click-and-collect/src/components/VFemme.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+<div class="container">    
+  <div class="row">
+    <div id="border" class="col-md-5">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Overdose des ailes</div>
+        <div class="panel-body"><img src="..\assets\Femmes\01-l-overdose-des-ailes.jpg" height="174" width="174" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer"><router-link class="" to="/">50 mobiles and get a gift card</router-link></div>
+      </div>
+    </div>
+    <div id="border" class="col-md-5"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading">BLACK FRIDAY DEAL</div>
+        <div class="panel-body"><img src="..\assets\Femmes\03-Parfum-du-colibri.png" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer"></div>
+      </div>
+    </div>
+  
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div id="border" class="col-md-5">
+      <div class="panel panel-primary">
+        <div class="panel-heading">BLACK FRIDAY DEAL</div>
+        <div class="panel-body"><img src="..\assets\Femmes\08-la-petite-robe-noir.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Découvrez l'Eau de toilette La Petite Robe Noire.
+          Ma Robe Cocktail
+          Dans le dressing parfumé de La Petite Robe Noire, je suis la Robe Cocktail</div>
+      </div>
+      <div>Prix : 150€</div>
+      <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
+    </div>
+    <div id="border" class="col-md-5"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading">BLACK FRIDAY DEAL</div>
+        <div class="panel-body"><img src="..\assets\Femmes\09-Shalimar.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">prix: <strong>15€</strong></div>
+      </div>
+    </div>
+    <div id="border" class="col-md-5"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading">BLACK FRIDAY DEAL</div>
+        <div class="panel-body"><img src="..\assets\Femmes\19-la-vie-est-Belle.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+      </div>
+    </div>
+  </div>
+</div><br>
+</div>
+
+
+
+</template>
+
+<style>
+  #border {
+    border: 3px solid #2cbdae;
+    margin-right: 30px;
+    margin-top: 50px;
+    border-radius: 10px;
+  }
+</style>
+
