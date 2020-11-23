4f16b978fa04b22e776563986bbcb57b9933fdc5
BaptisteDantas <baptistedantaspro@gmail.com>
2020-11-23 15:49:44 +0100

    dossierProjet

--- click-and-collect/src/components/VHomme.vue
+++ click-and-collect/src/components/VHomme.vue
@@ -5,20 +5,20 @@
   <div class="row">
     <div id="border" class="col-md-5">
       <div class="panel panel-primary">
-        <div class="panel-heading">Gabrielle de Chanel - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\06-gabrielle-de-chanel.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">Composée par Olivier Polge, Gabrielle de Chanel essence est la fragrance d’une fleur imaginaire, facettée de jasmin, d’ylang-ylang, de fleur d’oranger et de la plus envoûtante de toutes les fleurs, la tubéreuse de grasse. Intensément féminine, Gabrielle de Chanel essence est la fragrance d’une femme solaire.</div>
-        <div><strong>Prix: 74€</strong></div>
+        <div class="panel-heading">Le Mâle de Jean-Paul Gaultier - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\11-LeMale-JeanPaulGautier.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Jean-Paul Gaultier propose un hommage à une figure symbolique qui l’inspire depuis toujours : le marin. Le Male, aussi viril que sexy, est une fragrance qui offre une vision anticonformiste de la masculinité. Un parfum pour homme au sillage extrêmement moderne.
+</div>
+        <div><strong>Prix: 48€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">La vie est belle de Lancôme - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\19-la-vie-est-Belle.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer" style="padding-top: 30px">Parfum de jasmin d'Arabie, de fleur d'oranger et d'essence de patchouli.
-          Embellissez votre vie avec La vie est belle, le parfum féminin iconique de Lancôme.</div>
-        <div style="margin-top: 40px"><strong>Prix: 62€</strong></div>
+        <div class="panel-heading">Le Beau de Jean-Paul Gaultier - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\14-LeBeau-JEAN-PAUL GAULTIER.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer" style="padding-top: 30px">Une Eau de Toilette pour homme construite autour d'une addiction tonka et bois de coco associée aux facettes fraîches de la bergamote. Un sillage frais et puissant pour un homme séduisant et ultra sexy.</div>
+        <div style="margin-top: 40px"><strong>Prix: 47€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
@@ -29,19 +29,19 @@
   <div class="row">
     <div id="border" class="col-md-5">
       <div class="panel panel-primary">
-        <div class="panel-heading">Trésor de Lancôme - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\20-Tresor-Lancome.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">L'amour est un trésor. Depuis sa création en 1990, l'Eau de parfum Trésor incarne l'amour absolu sous toutes ses facettes. Une fragrance subtile et légère qu'enveloppe un voile doux et romantique</div>
+        <div class="panel-heading">Man de Hugo Boss - 125ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\12-MAN-Hugo-Boss.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Un nouveau parfum original Hugo Now qui associe des notes fraîches et aromatiques de cardamome, de zeste de citron, de lavande, de menthe et de vétiver. Cette fragrance vivifiante est parfaite pour les hommes à l’esprit aventurier qui vivent l’instant présent.</div>
       </div>
-      <div style="margin-top: 80px"><strong>Prix: 120€</strong></div>
+      <div style="margin-top: 80px"><strong>Prix: 83€</strong></div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">Olympéa de Paco Rabanne - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\24-Olympea-PacoRabanne.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">Déesse des temps modernes, Un parfum oriental frais, puissant, porté par un accord "vanille salée" qui adore la peau. Et un duel frais-charnel : impulsion florale avec la mandarine verte, le jasmin d'eau et la fleur de gingembre. Effet "peau" de la Vanille salée. Un parfum pour femme sensuel, exponentiel, presque animal. Chauffé par l'ambregris et le bois de Cachemire. Fatal.</div>
-        <div style="margin-top: 10px"><strong>Prix: 59€</strong></div>
+        <div class="panel-heading">L'Homme d'Yves Rocher - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\17-Lhomme-Yves-Sain-Laurent.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">La fragrance délivre des notes épicées, chaleureuses et enveloppantes, mêlées aux notes fraiches et délicatement boisées de la lavande.</div>
+        <div style="margin-top: 10px"><strong>Prix: 17€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
@@ -52,21 +52,21 @@
   <div class="row">
     <div id="border" class="col-md-5">
       <div class="panel panel-primary">
-        <div class="panel-heading">J'adore de Dior - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\25-j-adore-dior.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer">J’adore Eau de parfum de Dior : une fragrance mythique, véritable best seller de la Maison. J'adore compte parmi les parfums les plus vendus de la maison Dior</div>
+        <div class="panel-heading">1 Million de Paco Rabanne - 50ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\13-1Million-Paco-Rabanne.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer">Le parfum de l'insolence. Un sillage séduisant, irrésistible. Rien n’est trop Gold pour 1 Million.
+        Un bad boy qui voit la vie en GRAND. Sinon, à quoi bon ? N’obéit à personne. Sauf à ses fantasmes. Règle d’or.</div>
       </div>
-      <div style="margin-top: 70px"><strong>Prix: 45€</strong></div>
+      <div style="margin-top: 70px"><strong>Prix: 70€</strong></div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
       <div class="panel panel-primary">
-        <div class="panel-heading">La petite robe en noire - 35ml</div>
-        <div class="panel-body"><img src="..\assets\Femmes\08-la-petite-robe-noir.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
-        <div class="panel-footer" style="padding-top: 30px">Dans le dressing parfumé de La Petite Robe Noire, je suis la Première Robe.
-          Une création parfumée signée Guerlain : une eau de parfum brodée des plus belles notes "noires" de la parfumerie : Cerise noire, Rose noire, Patchouli, Thé noir.
+        <div class="panel-heading">Boss de Hugo Boss - 35ml</div>
+        <div class="panel-body"><img src="..\assets\Hommes\10-BOSS-Hugo-Boss.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
+        <div class="panel-footer" style="padding-top: 30px">La marque Hugo Boss est devenue synonyme d’une mode vestimentaire, d’accessoires et de produits cosmétiques masculins de grande qualité et de luxe. Dès sa création, elle a rapidement été recherchée pour ses tissus de qualité, mais aussi pour son sens du détail et de la précision.
           </div>
-        <div style="margin-top: 15px"><strong>Prix: 69€</strong></div>
+        <div style="margin-top: 15px"><strong>Prix: 24€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
