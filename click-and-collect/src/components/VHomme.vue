3d33ec2b68dee9ada0571bbe14a94cf3be6a2082
BaptisteDantas <baptistedantaspro@gmail.com>
2020-11-23 15:58:10 +0100

    Update VHomme.vue

--- click-and-collect/src/components/VHomme.vue
+++ click-and-collect/src/components/VHomme.vue
@@ -9,7 +9,7 @@
         <div class="panel-body"><img src="..\assets\Hommes\11-LeMale-JeanPaulGautier.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
         <div class="panel-footer">Jean-Paul Gaultier propose un hommage à une figure symbolique qui l’inspire depuis toujours : le marin. Le Male, aussi viril que sexy, est une fragrance qui offre une vision anticonformiste de la masculinité. Un parfum pour homme au sillage extrêmement moderne.
 </div>
-        <div><strong>Prix: 48€</strong></div>
+        <div style="margin-top: 50px"><strong>Prix: 48€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
@@ -33,7 +33,7 @@
         <div class="panel-body"><img src="..\assets\Hommes\12-MAN-Hugo-Boss.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
         <div class="panel-footer">Un nouveau parfum original Hugo Now qui associe des notes fraîches et aromatiques de cardamome, de zeste de citron, de lavande, de menthe et de vétiver. Cette fragrance vivifiante est parfaite pour les hommes à l’esprit aventurier qui vivent l’instant présent.</div>
       </div>
-      <div style="margin-top: 80px"><strong>Prix: 83€</strong></div>
+      <div style="margin-top: 20px"><strong>Prix: 83€</strong></div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
@@ -41,7 +41,7 @@
         <div class="panel-heading">L'Homme d'Yves Rocher - 35ml</div>
         <div class="panel-body"><img src="..\assets\Hommes\17-Lhomme-Yves-Sain-Laurent.jpg" class="img-responsive" style="width:50%" alt="Image"></div>
         <div class="panel-footer">La fragrance délivre des notes épicées, chaleureuses et enveloppantes, mêlées aux notes fraiches et délicatement boisées de la lavande.</div>
-        <div style="margin-top: 10px"><strong>Prix: 17€</strong></div>
+        <div style="margin-top: 70px"><strong>Prix: 17€</strong></div>
       </div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
@@ -57,7 +57,7 @@
         <div class="panel-footer">Le parfum de l'insolence. Un sillage séduisant, irrésistible. Rien n’est trop Gold pour 1 Million.
         Un bad boy qui voit la vie en GRAND. Sinon, à quoi bon ? N’obéit à personne. Sauf à ses fantasmes. Règle d’or.</div>
       </div>
-      <div style="margin-top: 70px"><strong>Prix: 70€</strong></div>
+      <div style="margin-top: 90px"><strong>Prix: 70€</strong></div>
       <button type="button" class="btn btn-success" style="margin-bottom: 10px">Ajouter au panier</button>
     </div>
     <div id="border" class="col-md-5"> 
