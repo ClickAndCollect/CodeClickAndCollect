4803a7893ff2535686160c2ee31bb8331a125300
BaptisteDantas <baptistedantaspro@gmail.com>
2020-11-25 16:17:49 +0100

    dossierProjet

--- click-and-collect/src/components/VPaiement.vue
+++ click-and-collect/src/components/VPaiement.vue
@@ -1,155 +1,201 @@
 <template>
   <div>
-    <fieldset>
-      <legend>Informations CB</legend>
-      <ol>
-        <li>
-          <fieldset>
-            <legend>Type de carte bancaire</legend>
-            <ul>
-              <li>
-                <input id="visa" name="type_de_carte" type="radio" />
-                <label for="visa">VISA</label>
-              </li>
-              <li>
-                <input id="amex" name="type_de_carte" type="radio" />
-                <label for="amex">American Express</label>
-              </li>
-              <li>
-                <input id="mastercard" name="type_de_carte" type="radio" />
-                <label for="mastercard">Mastercard</label>
-              </li>
-            </ul>
-          </fieldset>
-        </li>
-        <li>
-          <label for="numero_de_carte">Numéro de carte</label>
-          <input
-            id="numero_de_carte"
-            name="numero_de_carte"
-            type="number"
-            required
-          />
-        </li>
-        <li>
-          <label for="securite">Code sécurité</label>
-          <input id="securite" name="securite" type="text" placeholder="Au dos de la carte" required />
-        </li>
-        <li>
-          <label for="securite">Date de validité</label>
-          <input name="date" type="date" required />
-        </li>
-        <li>
-          <label for="nom_porteur">Nom sur la carte</label>
-          <input
-            id="nom_porteur"
-            name="nom_porteur"
-            type="number"
-            required
-          />
-        </li>
-      </ol>
-    </fieldset>
-
-    <fieldset>
-      <button type="submit">Payer</button>
-    </fieldset>
+    <h1 class="titre">Information bancaire</h1>
+    <div class="paiement">
+      <form
+        method="post"
+        data-centralpay="form"
+        action="https://example.centralpay.net/v2/process/display"
+      >
+        <div class="row mx-0 text-center"></div>
+        <div data-centralpay="errors" class="custom-form"></div>
+        <p data-form="main-description" class="sr-only">Card data</p>
+        <div class="form-row mt-3" data-toggle="popover-example-data">
+          <div class="form-group col-6">
+            <div class="has-feedback input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">
+                  <div class="ico ico-user"></div>
+                </div>
+              </div>
+              <input
+                data-centralpay="firstName"
+                type="text"
+                name="order[firstName]"
+                autocomplete="off"
+                data-form="card-user"
+                class="form-control"
+                placeholder="First name"
+                required="required"
+              />
+            </div>
+          </div>
+          <div class="form-group col-6">
+            <div class="has-feedback input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">
+                  <div class="ico ico-user"></div>
+                </div>
+              </div>
+              <input
+                data-centralpay="lastName"
+                type="text"
+                name="order[lastName]"
+                autocomplete="off"
+                data-form="card-user"
+                class="form-control"
+                placeholder="Last name"
+                required="required"
+              />
+            </div>
+          </div>
+        </div>
+        <div data-form="card-validation">
+          <div class="form-row d-none">
+            <div class="form-group col-12">
+              <select
+                list="userCards"
+                data-form="card-list"
+                type="text"
+                class="form-control input-large"
+              >
+                <option>-- pay with a new credit card --</option>
+              </select>
+            </div>
+          </div>
+          <div id="card-option" class="new-card-ok">
+            <div class="form-row">
+              <div class="form-group col-12 new-card">
+                <div class="has-feedback input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <div class="ico ico-cb"></div>
+                    </div>
+                  </div>
+                  <input
+                    data-centralpay="card[number]"
+                    type="tel"
+                    pattern="([0-9]{16})"
+                    autocomplete="off"
+                    class="form-control form-card-data form-card-number"
+                    placeholder="Numéro de carte"
+                    maxlength="19"
+                    data-form="card-data"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="form-row align-items-center">
+              <div class="form-group col-8 new-card">
+                <div class="has-feedback input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <div class="ico ico-calendar"></div>
+                    </div>
+                  </div>
+                  <input
+                    data-centralpay="card[expirationMonth]"
+                    type="tel"
+                    pattern="[0-9]{1,2}"
+                    maxlength="2"
+                    autocomplete="off"
+                    class="form-control form-card-data"
+                    placeholder="MM"
+                    data-form="card-data"
+                  />
+                  <input
+                    data-centralpay="card[expirationYear]"
+                    type="tel"
+                    max="9999"
+                    inputmode="tel"
+                    pattern="[0-9]{4}"
+                    maxlength="4"
+                    autocomplete="off"
+                    class="form-control form-card-data"
+                    data-form="card-data"
+                    placeholder="YYYY"
+                  />
+                </div>
+              </div>
+              <div class="form-group stored-card use-cvv text-right"></div>
+              <div class="form-group col-4 use-cvv">
+                <div class="has-feedback input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <div class="ico ico-lock"></div>
+                    </div>
+                  </div>
+                  <input
+                    data-centralpay="card[cvc]"
+                    type="tel"
+                    pattern="[0-9]{3}"
+                    maxlength="3"
+                    autocomplete="off"
+                    class="form-control form-card-data"
+                    placeholder="Code sécurité (au dos)"
+                    name="customerValidationValue"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="col-12">
+            <div
+              data-form="remember-div"
+              class="remember remember-option d-flex align-items-center mb-3"
+            >
+              <span class="remember-checkbox form-check">
+                <input
+                  class="form-check-input"
+                  data-form="remember-select"
+                  id="remember"
+                  type="checkbox"
+                  name="store-card"
+                />
+                <label class="form-check-label" for="remember"
+                  >Se souvenir de ma carte</label
+                >
+              </span>
+              <a class="help-icon" data-toggle="popover-info">
+                <img src="/v2/src/img/question-mark.png" alt="" />
+              </a>
+              <div id="popover-card-info" class="d-none">
+                <span class="np-popover-style"
+                  >Your card data is stored securely.
+                  <a href="#">More information</a></span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="text-center mt-2">
+          <button
+            class="btn btn-cpay btn-success"
+            type="submit"
+            value="submit"
+            data-form="submit"
+            data-toggle="popover"
+            data-placement="bottom"
+            data-trigger="hover"
+          >
+            <div class="np-button-content">
+              <span data-display="submit-value">Payer</span>
+              <div class="np-loader"></div>
+            </div>
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <style>
-html, body, h1, form, fieldset, legend, ol, li {
-  margin: 0;
-  padding: 0;
+.titre {
+  padding-top: 30px;
 }
-li {
-  list-style: none;
-}
-form#paiement {
-  background: #9cbc2c;
-  -moz-border-radius: 5px;
-  -webkit-border-radius: 5px;
-  border-radius: 5px;
-  padding: 20px;
-  width: 400px;
-}
-form#paiement fieldset {
-  border: none;
-  margin-bottom: 10px;
-}
-form#paiement fieldset:last-of-type {
-  margin-bottom: 0;
-}
-form#paiement legend {
-  color: #384313;
-  font-size: 16px;
-  font-weight: bold;
-  padding-bottom: 10px;
-  text-shadow: 0 1px 1px #c0d576;
-}
-form#paiement > fieldset > legend:before {
-  content: "Étape " counter(fieldsets) " : ";
-  counter-increment: fieldsets;
-}
-form#paiement fieldset fieldset legend {
-  color: #111111;
-  font-size: 13px;
-  font-weight: normal;
-  padding-bottom: 0;
-}
-form#paiement label {
-  float: left;
-  font-size: 13px;
-  width: 110px;
-}
-form#paiement fieldset fieldset label {
-  background:none no-repeat left 50%;
-  line-height: 20px;
-  padding: 0 0 0 30px;
-  width: auto;
-}
-form#paiement fieldset fieldset label:hover {
-  cursor: pointer;
-}
-form#paiement input:not([type=radio]),
-form#paiement textarea {
-  background: #ffffff;
-  border: none;
-  -moz-border-radius: 3px;
-  -webkit-border-radius: 3px;
-  -khtml-border-radius: 3px;
-  border-radius: 3px;
-  font: italic 13px Georgia, "Times New Roman", Times, serif;
-  outline: none;
-  padding: 5px;
-  width: 200px;
-}
-form#paiement input:not([type=submit]):focus,
-form#paiement textarea:focus {
-  background: #eaeaea;
-}
-form#paiement input[type=radio] {
-  float: left;
-  margin-right: 5px;
-}
-form#paiement button {
-  background: #384313;
-  border: none;
-  -moz-border-radius: 20px;
-  -webkit-border-radius: 20px;
-  -khtml-border-radius: 20px;
-  border-radius: 20px;
-  color: #ffffff;
-  display: block;
-  font: 18px Georgia, "Times New Roman", Times, serif;
-  letter-spacing: 1px;
-  margin: auto;
-  padding: 7px 25px;
-  text-shadow: 0 1px 1px #000000;
-  text-transform: uppercase;
-}
-form#paiement button:hover {
-  background: #1e2506;
-  cursor: pointer;
+.paiement {
+  padding-top: 100px;
 }
 </style>
